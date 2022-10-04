b18fb908f5734504f9efc13614ca2ee22724565f
Anshul <anshul3745@gmail.com>
2022-10-03 21:22:43 -0400

    completed console application

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -10,7 +10,55 @@ namespace Assignment02
     {
         static void Main(string[] args)
         {
+            Rectangle rectangle = new Rectangle();
+            int option = 0;
 
+            Console.WriteLine();
+            Console.WriteLine("1 = Get Rectangle Length");
+            Console.WriteLine("2 = Change Rectangle Length");
+            Console.WriteLine("3 = Get Rectangle Width");
+            Console.WriteLine("4 = Change Rectangle Width");
+            Console.WriteLine("5 = Get Rectangle perimeter");
+            Console.WriteLine("6 = Get Rectangle Area");
+            Console.WriteLine("7 = Exit");
+
+            Console.WriteLine();
+            Console.WriteLine("Please select any option to perform Operation : ");
+
+            while (option != 8)
+            {
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        rectangle.GetCurrentLength();
+                        break;
+                    case 2:
+                        Console.WriteLine("Please enter rectangle length to change:");
+                        int newLength = Convert.ToInt32(Console.ReadLine());
+                        rectangle.SetNewLength(newLength);
+                        break;
+                    case 3:
+                        rectangle.GetCurrentWidth();
+                        break;
+                    case 4:
+                        Console.WriteLine("Please enter rectangle width to change:");
+                        int newWidth = Convert.ToInt32(Console.ReadLine());
+                        rectangle.SetNewWidth(newWidth);
+                        break;
+                    case 5:
+                        rectangle.GetPerimeter();
+                        break;
+                    case 6:
+                        rectangle.GetArea();
+                        break;
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
