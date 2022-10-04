9642eaa56d9cd35f7fa5da7a14af7e13f2e330e1
Anshul <anshul3745@gmail.com>
2022-10-03 21:04:47 -0400

    completed rectangle class

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -10,6 +10,7 @@ namespace Assignment02
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ Assignment02/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    internal class Rectangle
+    {
+        private static int length;
+        private static int width;
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int newLength, int newWidth)
+        {
+            SetNewLength(newLength);
+            SetNewWidth(newWidth);
+        }
+        public int GetCurrentLength()
+        {
+            Console.WriteLine("Current Rectangle Length is:" + length);
+            return length;
+        }
+        public int SetNewLength(int newLength)
+        {
+            length = newLength;
+            return length;
+        }
+        public int GetCurrentWidth()
+        {
+            Console.WriteLine("Current Rectangle Width is:" + width);
+            return width;
+        }
+        public int SetNewWidth(int newWidth)
+        {
+            width = newWidth;
+            return width;
+        }
+        public int GetPerimeter()
+        {
+            int rectanglePerimeter = 2 * (length + width);
+            Console.WriteLine("Perimeter of Rectangle is : " + rectanglePerimeter);
+            return rectanglePerimeter;
+        }
+        public int GetArea()
+        {
+            int rectangleArea = length * width;
+            Console.WriteLine("Area of Rectangle is :" + rectangleArea);
+            return rectangleArea;
+        }
+    }
+}
